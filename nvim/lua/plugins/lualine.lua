f0f6b064ec2c106223b8d38f076a9dbf54f57644
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2024-09-19 10:17:07 +0200

    Update telescope and lua line config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,15 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "dracula",
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "dracula",
+      },
+      sections = {
+        lualine_c = { { "filename", path = 3 } },
+        lualine_x = { "filetype" },
+        lualine_y = { "encoding", "fileformat" },
+      },
+    })
+  end,
 }
