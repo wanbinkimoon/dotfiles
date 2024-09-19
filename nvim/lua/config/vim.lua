aedc049a42e3c533c4baea6cd65a6b1afc00e4b0
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2024-09-19 10:15:51 +0200

    Update vim options

--- nvim/lua/config/vim.lua
+++ nvim/lua/config/vim.lua
@@ -6,6 +6,11 @@ vim.g.mapleader = " "
 
 vim.opt.showmode = false
 vim.opt.swapfile = false
+vim.opt.breakindent = true
+vim.opt.undofile = true
+-- vim.opt.cursorline = true
+vim.wo.number = true
+vim.opt.scrolloff = 10
 
 -- Navigate vim panes better
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
@@ -13,18 +18,12 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.wo.number = true
-
--- vim.opt.cursorline = true
-
-vim.opt.scrolloff = 10
-
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
