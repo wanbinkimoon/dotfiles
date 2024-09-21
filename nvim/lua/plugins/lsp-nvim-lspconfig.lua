3fbbc06134975571d344e06881cf098d307f6748
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2024-09-22 00:32:26 +0200

    Refactor nvim plugins

---
+++ nvim/lua/plugins/lsp-nvim-lspconfig.lua
@@ -0,0 +1,39 @@
+return {
+	"neovim/nvim-lspconfig",
+	lazy = false,
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		local lspconfig = require("lspconfig")
+
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+		})
+
+		lspconfig.html.setup({
+			capabilities = capabilities,
+		})
+
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+		})
+
+		lspconfig.yamlls.setup({
+			capabilities = capabilities,
+		})
+
+		lspconfig.jsonls.setup({
+			capabilities = capabilities,
+		})
+
+		lspconfig.cssls.setup({
+			capabilities = capabilities,
+		})
+
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "<leader>cgd", vim.lsp.buf.definition, { desc = "[C]ode: [G]o to [D]efinition" })
+		vim.keymap.set("n", "<leader>cgr", vim.lsp.buf.references, { desc = "[C]ode: [G]o to [R]eference" })
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode: [A]ction" })
+		vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "[C]ode: [R]ename" })
+	end,
+}
