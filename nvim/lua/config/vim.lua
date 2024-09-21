b341fc0030715d02e2c30e9c6e7c8999f6464c9d
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2024-09-22 01:06:18 +0200

    Update .gitignore

--- nvim/lua/config/vim.lua
+++ nvim/lua/config/vim.lua
@@ -21,9 +21,22 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
+
+-- vim.api.nvim_create_augroup("neotree", {})
+-- vim.api.nvim_create_autocmd("UiEnter", {
+-- 	desc = "Open Neotree automatically",
+-- 	group = "neotree",
+-- 	callback = function()
+-- 		if vim.fn.argc() == 0 then
+-- 			vim.cmd("Neotree filesystem toggle")
+-- 		end
+-- 	end,
+-- })
+
+vim.cmd([[autocmd VimEnter * Neotree]])
