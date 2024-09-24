ffacd0d13d5f24cfe52afdbe1e71de314f81300c
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2024-09-24 09:18:15 +0200

    Add keymaps to move lines

--- nvim/lua/config/vim.lua
+++ nvim/lua/config/vim.lua
@@ -18,6 +18,12 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
+-- Move lines up and down
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==")
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==")
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
