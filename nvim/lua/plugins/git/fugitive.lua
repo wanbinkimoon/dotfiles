01d23e160bb8a1ab94482a955b978857f681b59d
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2024-09-26 23:24:00 +0200

    Add fugitive keymaps

--- nvim/lua/plugins/git/fugitive.lua
+++ nvim/lua/plugins/git/fugitive.lua
@@ -1,11 +1,15 @@
 return {
   "tpope/vim-fugitive", -- Git wrapper
   keys = {
-    { "<leader>gs", "<cmd>:Git<CR>",       desc = "[G]it: open git status" },
+    { "<leader>gs", "<cmd>:Git<CR>",         desc = "[G]it: open git status" },
     -- { "<leader>gc", "<cmd>:Git commit<CR>", desc = "[G]it: commit" },
     -- { "<leader>gp", "<cmd>:Git push<CR>", desc = "[G]it: push" },
     -- { "<leader>gl", "<cmd>:Git pull<CR>", desc = "[G]it: pull" },
-    { "<leader>gb", "<cmd>:Git blame<CR>", desc = "[G]it: blame" },
+    { "<leader>gb", "<cmd>:Git blame<CR>",   desc = "[G]it: blame" },
+    { "<leader>gd", "<cmd>:Gdiff<CR>",       desc = "[G]it: diff" },
+    { "<leader>gD", "<cmd>:Gdiffsplit<CR>",  desc = "[G]it: diff split" },
+    { "<leader>gk", "<cmd>:diffget //3<CR>", desc = "[G]it: diff get from right" },
+    { "<leader>gj", "<cmd>:diffget //2<CR>", desc = "[G]it: diff get from left" },
   },                                           -- optional
   config = function()
     vim.g.fugitive_git_command = "git"         -- Use system git
