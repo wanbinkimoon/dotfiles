af8879cda806ea3aef3fa3a0dd93d77dec38e07e
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2024-10-02 00:02:39 +0200

    Lualine add tabs

--- nvim/lua/plugins/ui/lualine.lua
+++ nvim/lua/plugins/ui/lualine.lua
@@ -1,23 +1,36 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = {
-    { "dokwork/lualine-ex" },
-  },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "dracula",
-        component_separators = "/",
-        section_separators = { left = "", right = "" },
-        -- disabled_filetypes = { "neo-tree" },
-        globalstatus = true,
-      },
-      sections = {
-        -- lualine_c = { { "filename", path = 3 } },
-        lualine_c = { { "ex.relative_filename", max_length = 0.8 } },
-        lualine_x = { "filetype" },
-        lualine_y = { "encoding" },
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		{ "dokwork/lualine-ex" },
+	},
+	config = function()
+		local dracula = require("dracula").colors()
+
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+				component_separators = "/",
+				section_separators = { left = "", right = "" },
+				-- disabled_filetypes = { "neo-tree" },
+				globalstatus = true,
+			},
+			sections = {
+				-- lualine_c = { { "filename", path = 3 } },
+				lualine_c = { { "ex.relative_filename", max_length = 0.8 } },
+				lualine_x = {
+					"filetype",
+					{
+						"tabs",
+						mode = 1,
+						symbols = { modified = "󱗝" },
+						tabs_color = {
+							active = { fg = dracula.bright_blue },
+							inactive = { fg = dracula.purple },
+						},
+					},
+				},
+				lualine_y = { "encoding" },
+			},
+		})
+	end,
 }
