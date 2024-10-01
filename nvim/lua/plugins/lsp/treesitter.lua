d1c627a6da7a7f038721dfa0c3786071fd89fdb3
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2024-10-02 00:04:52 +0200

    Fix lsp missing langs

--- nvim/lua/plugins/lsp/treesitter.lua
+++ nvim/lua/plugins/lsp/treesitter.lua
@@ -1,12 +1,48 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
+			-- ensure these language parsers are installed
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"vimdoc",
+				"c",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
 		})
 	end,
 }
