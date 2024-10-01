308204588f357615dde64c50dcad1857e03b8648
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2024-10-02 00:02:55 +0200

    Tweaking neo-tree to hide DS_Store

--- nvim/lua/plugins/ui/neo-tree.lua
+++ nvim/lua/plugins/ui/neo-tree.lua
@@ -1,28 +1,29 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			filesystem = {
-				filtered_items = {
-					visible = true,
-					show_hidden_count = true,
-					hide_dotfiles = false,
-					hide_gitignored = false,
-				},
-				follow_current_file = {
-					enabled = true,
-					leave_dirs_open = false,
-				},
-			},
-			buffers = { follow_current_file = { enable = true } },
-		})
-		vim.keymap.set("n", "<leader>1", ":Neotree filesystem toggle left<CR>", {})
-		vim.keymap.set("n", "<leader>2", ":Neotree buffers reveal float<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    local opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = { ".git" },
+          never_show = { ".DS_Store" },
+          always_show = { ".env" },
+        },
+        follow_current_file = { leave_dirs_open = false, enabled = true },
+      },
+      buffers = { follow_current_file = { enable = true } },
+    }
+    require("neo-tree").setup(opts)
+    vim.keymap.set("n", "<leader>1", ":Neotree filesystem toggle left<CR>", {})
+    vim.keymap.set("n", "<leader>2", ":Neotree buffers reveal float<CR>", {})
+  end,
 }
