f4c70958f2c610ba058f9f5657f8433b550e80ed
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2024-10-08 00:31:24 +0200

    chore: Provided missing description for commads

--- nvim/lua/plugins/search/grug-far.lua
+++ nvim/lua/plugins/search/grug-far.lua
@@ -1,9 +1,13 @@
 return {
-	"MagicDuck/grug-far.nvim",
-	keys = {
-		{ "<leader>sr", "<cmd>:lua require('grug-far').open()<CR>", desc = "[S]earch: search and replace" },
-	},
-	config = function()
-		require("grug-far").setup({})
-	end,
+  "MagicDuck/grug-far.nvim",
+  keys = {
+    {
+      "<leader>sr",
+      "<cmd>:lua require('grug-far').open({ transient = true })<CR>",
+      desc = "[S]earch: search and replace",
+    },
+  },
+  config = function()
+    require("grug-far").setup({})
+  end,
 }
