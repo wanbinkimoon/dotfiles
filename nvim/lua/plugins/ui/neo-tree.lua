0cdf1097c1c7fb14327648d8ed8eb12feb494a46
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2024-10-26 12:18:02 +0200

    chore: refactor neotree and error les for git and diagnostics

--- nvim/lua/plugins/ui/neo-tree.lua
+++ nvim/lua/plugins/ui/neo-tree.lua
@@ -5,10 +5,28 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
+    "sindrets/diffview.nvim",
   },
   config = function()
+    local icons = require("config.icons")
     local opts = {
       close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+      enable_git_status = true,
+      enable_diagnostics = false,
+      indent = {
+        indent_size = 2,
+        padding = 0, -- extra padding on left hand side
+        -- indent guides
+        -- with_markers = true,
+        -- indent_marker = "│",
+        -- last_indent_marker = "└",
+        -- highlight = "NeoTreeIndentMarker",
+        -- expander config, needed for nesting files
+        -- with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+        -- expander_collapsed = "",
+        -- expander_expanded = "",
+        -- expander_highlight = "NeoTreeExpander",
+      },
       filesystem = {
         filtered_items = {
           visible = true,
@@ -35,10 +53,57 @@ return {
         ["/"] = "fuzzy_finder",
         ["f"] = "filter_on_submit",
       },
+      git_status = {
+        window = {
+          position = "float",
+          mappings = {
+            ["A"] = "git_add_all",
+            ["gu"] = "git_unstage_file",
+            ["ga"] = "git_add_file",
+            ["gr"] = "git_revert_file",
+            ["gc"] = "git_commit",
+            ["gp"] = "git_push",
+            ["gg"] = "git_commit_and_push",
+
+            ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+            ["oc"] = { "order_by_created", nowait = false },
+            ["od"] = { "order_by_diagnostics", nowait = false },
+            ["om"] = { "order_by_modified", nowait = false },
+            ["on"] = { "order_by_name", nowait = false },
+            ["os"] = { "order_by_size", nowait = false },
+            ["ot"] = { "order_by_type", nowait = false },
+          },
+        },
+      },
+      default_component_configs = {
+        modified = {
+          -- symbol = "",
+          symbol = "",
+          highlight = "NeoTreeModified",
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = icons.git.Add,
+            deleted = icons.git.Remove,
+            modified = icons.git.Mod,
+            renamed = icons.git.Rename,
+            -- Status type
+            untracked = icons.git.Untrack,
+            ignored = icons.git.Ignore,
+            unstaged = "",
+            -- staged = icons.git.Staged,
+            staged = "",
+            conflict = icons.git.Conflict,
+          },
+          align = "right",
+        },
+      },
     }
     require("neo-tree").setup(opts)
 
     vim.keymap.set("n", "<leader>e", ":Neotree filesystem toggle left<CR>", { desc = "Neotree [e]xplore" })
     vim.keymap.set("n", "<leader>b", ":Neotree buffers reveal float<CR>", { desc = "NeoTree [b]uffers" })
+    vim.keymap.set("n", "<leader>G", ":Neotree float git_status<CR>", { desc = "NeoTree [G]it status" })
   end,
 }
---
+++ nvim/lua/plugins/ui/statuscol.lua
@@ -0,0 +1,91 @@
+return {
+	{
+		"luukvbaal/statuscol.nvim",
+		dependencies = {
+			{ "lewis6991/gitsigns.nvim" },
+		},
+		config = function()
+			require("statuscol").setup({})
+			local builtin = require("statuscol.builtin")
+
+			local cfg = {
+				setopt = true, -- Whether to set the 'statuscolumn' option, may be set to false for those who
+				-- want to use the click handlers in their own 'statuscolumn': _G.Sc[SFL]a().
+				-- Although I recommend just using the segments field below to build your
+				-- statuscolumn to benefit from the performance optimizations in this plugin.
+				-- builtin.lnumfunc number string options
+				thousands = false, -- or line number thousands separator string ("." / ",")
+				relculright = true, -- whether to right-align the cursor line number with 'relativenumber' set
+				-- Builtin 'statuscolumn' options
+				ft_ignore = nil, -- lua table with 'filetype' values for which 'statuscolumn' will be unset
+				bt_ignore = nil, -- lua table with 'buftype' values for which 'statuscolumn' will be unset
+				segments = {
+					{
+						text = { builtin.foldfunc, "  " },
+						click = "v:lua.ScFa",
+						hl = "Comment",
+					},
+					{ text = { "%s" }, click = "v:lua.ScSa" },
+					{
+						sign = { name = { "GitSign*" } },
+						click = "v:lua.ScSa",
+					},
+					{
+						text = { builtin.lnumfunc, "  " },
+						condition = { true, builtin.not_empty },
+						click = "v:lua.ScLa",
+					},
+				},
+				clickmod = "c", -- modifier used for certain actions in the builtin clickhandlers:
+				-- "a" for Alt, "c" for Ctrl and "m" for Meta.
+				clickhandlers = { -- builtin click handlers
+					Lnum = builtin.lnum_click,
+					FoldClose = builtin.foldclose_click,
+					FoldOpen = builtin.foldopen_click,
+					FoldOther = builtin.foldother_click,
+					DapBreakpointRejected = builtin.toggle_breakpoint,
+					DapBreakpoint = builtin.toggle_breakpoint,
+					DapBreakpointCondition = builtin.toggle_breakpoint,
+					DiagnosticSignError = builtin.diagnostic_click,
+					DiagnosticSignHint = builtin.diagnostic_click,
+					DiagnosticSignInfo = builtin.diagnostic_click,
+					DiagnosticSignWarn = builtin.diagnostic_click,
+					GitSignsTopdelete = builtin.gitsigns_click,
+					GitSignsUntracked = builtin.gitsigns_click,
+					GitSignsAdd = builtin.gitsigns_click,
+					GitSignsChange = builtin.gitsigns_click,
+					GitSignsChangedelete = builtin.gitsigns_click,
+					GitSignsDelete = builtin.gitsigns_click,
+					gitsigns_extmark_signs_ = builtin.gitsigns_click,
+				},
+				fold = {
+					width = 1, -- current width of the fold column
+					-- 'fillchars' option values:
+					close = "", -- foldclose
+					open = "", -- foldopen
+					sep = " ", -- foldsep
+				},
+			}
+
+			require("statuscol").setup(cfg)
+
+			local id = vim.api.nvim_create_augroup("statuscol", {})
+			vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
+				group = id,
+				callback = function()
+					if vim.api.nvim_buf_get_option(0, "ft") == "NvimTree" then
+						vim.opt_local.statuscolumn = ""
+					end
+
+					if vim.api.nvim_buf_get_option(0, "ft") == "aerial" then
+						vim.opt_local.statuscolumn = " "
+					end
+
+					if vim.api.nvim_buf_get_option(0, "ft") == "Outline" then
+						vim.opt_local.statuscolumn = " "
+					end
+				end,
+			})
+		end,
+	},
+}
