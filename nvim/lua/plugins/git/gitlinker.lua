ed92f5fbee84c8d45861f4ed8ea668d176a03333
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2024-10-26 12:15:16 +0200

    fix: git linker keymap

--- nvim/lua/plugins/git/gitlinker.lua
+++ nvim/lua/plugins/git/gitlinker.lua
@@ -13,13 +13,13 @@ return {
 
 			vim.keymap.set(
 				"n",
-				"<leader>gb",
+				"<leader>gy",
 				'<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
 				{ silent = true, desc = "GitLinker: open in browser" }
 			)
 			vim.keymap.set(
 				"v",
-				"<leader>gb",
+				"<leader>gy",
 				'<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
 				{ desc = "GitLinker: open in browser" }
 			)
