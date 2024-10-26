b7b103a258b36a3b8f9839c72a9209b420ffa441
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2024-10-26 12:14:30 +0200

    feat: add nvim zen mode

---
+++ nvim/lua/plugins/editor/zen-mode.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/zen-mode.nvim",
+  opts = {
+    backdrop = 1,
+    gitsigns = {
+      enabled = false,
+    }, -- disables git signs
+    tmux = {
+      enabled = true,
+    }, -- disables the tmux statusline
+    wezterm = {
+      enabled = true,
+      -- can be either an absolute font size or the number of incremental steps
+      font = "+4", -- (10% increase per step)
+    },
+  },
+}
