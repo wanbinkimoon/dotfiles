b256024640507b36e3658b8f2a54a80b20a1e814
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2024-10-26 12:18:39 +0200

    chore: update cpm for better completion shortcuts

--- nvim/lua/plugins/code/cpm-nvim.lua
+++ nvim/lua/plugins/code/cpm-nvim.lua
@@ -1,16 +1,15 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
+
   {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+      "dcampos/cmp-emmet-vim",
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/cmp-nvim-lsp",
+
+      "rafamadriz/friendly-snippets",
+      "saadparwaiz1/cmp_luasnip",
+    },
     config = function()
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
@@ -41,6 +40,7 @@ return {
           { name = "luasnip" }, -- For luasnip users.
         }, {
           { name = "buffer" },
+          { name = "emmet_vim" },
         }),
       })
     end,
