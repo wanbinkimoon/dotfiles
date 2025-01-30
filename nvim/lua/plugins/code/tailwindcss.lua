3a717903e85820f44eb9f2b4500e9f5d384c994e
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2025-01-30 15:38:42 +0100

    feat: implement tailwind plugin

---
+++ nvim/lua/plugins/code/tailwindcss.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"roobert/tailwindcss-colorizer-cmp.nvim",
+		-- optionally, override the default options:
+		config = function()
+			require("tailwindcss-colorizer-cmp").setup({
+				color_square_width = 2,
+			})
+		end,
+	},
+	{
+		"luckasRanarison/tailwind-tools.nvim",
+		name = "tailwind-tools",
+		build = ":UpdateRemotePlugins",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim", -- optional
+			"neovim/nvim-lspconfig", -- optional
+		},
+		opts = {}, -- your configuration
+	},
+}
