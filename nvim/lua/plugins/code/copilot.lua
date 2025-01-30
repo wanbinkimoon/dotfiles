cfc97ec97904bb9cdc1b54ea6ad995b95cff70e9
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2025-01-30 15:37:49 +0100

    chore: restore inline copilot

--- nvim/lua/plugins/code/copilot.lua
+++ nvim/lua/plugins/code/copilot.lua
@@ -1,17 +1,23 @@
 return {
 	{
+		"github/copilot.vim",
+	},
+	{
 		"zbirenbaum/copilot.lua",
 		lazy = true,
 		event = "BufReadPre",
 		config = function()
 			require("copilot").setup({
-				suggestion = { enabled = false },
+				suggestion = { enabled = true },
 				panel = { enabled = false },
 			})
 		end,
 	},
 	{
 		"zbirenbaum/copilot-cmp",
+		enabled = false,
+		lazy = true,
+		event = "BufReadPre",
 		config = function()
 			require("copilot_cmp").setup()
 		end,
