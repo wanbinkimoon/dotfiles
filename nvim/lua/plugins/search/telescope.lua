cc371c04f48ff2d64b837c95165092a4d9410c72
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2025-02-05 09:43:16 +0100

    feat(nvim): telescope for git history

--- nvim/lua/plugins/search/telescope.lua
+++ nvim/lua/plugins/search/telescope.lua
@@ -1,40 +1,181 @@
 return {
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
-	{
-		"mrloop/telescope-git-branch.nvim",
-	},
+	{ "nvim-telescope/telescope-ui-select.nvim" },
+	{ "mrloop/telescope-git-branch.nvim" },
 	{
 		"nvim-telescope/telescope.nvim",
-		-- tag = "0.1.5",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
 			{
-				"nvim-telescope/telescope-live-grep-args.nvim",
-				version = "^1.0.0",
+				"isak102/telescope-git-file-history.nvim",
+				dependencies = {
+					"nvim-lua/plenary.nvim",
+					"tpope/vim-fugitive",
+				},
 			},
 		},
 		config = function()
 			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+			local action_state = require("telescope.actions.state")
 
-			local options = {}
+			-- Unified dimensions for consistent sizing
+			local dimensions = {
+				width = 0.85,
+				height = 0.85,
+			}
 
-			-- Optione: Extensions
-			options.extensions = {
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown({}),
+			local options = {
+				defaults = {
+					layout_strategy = "horizontal",
+					layout_config = {
+						horizontal = {
+							height = dimensions.height,
+							width = dimensions.width,
+							preview_width = 0.5,
+							prompt_position = "top",
+							preview_position = "bottom",
+						},
+					},
+					sorting_strategy = "ascending",
+					-- Center results in the screen
+					results_title = false,
+					prompt_prefix = "   ",
+					selection_caret = "  ",
+					-- Initial state: no preview, normal mode
+					initial_mode = "insert",
+					-- No preview on startup
+					previewer = false,
+					preview = {
+						hide_on_startup = true,
+					},
+					mappings = {
+						n = {
+							["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+							["j"] = actions.move_selection_next,
+							["k"] = actions.move_selection_previous,
+							["<C-c>"] = actions.close,
+							["<CR>"] = actions.select_default,
+						},
+						i = {
+							["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+							["<C-c>"] = actions.close,
+							["<C-j>"] = actions.move_selection_next,
+							["<C-k>"] = actions.move_selection_previous,
+						},
+					},
+					-- Consistent theme across all pickers
+					borderchars = {
+						prompt = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+						results = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+						preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+					},
+					-- Better highlighting
+					highlights = {
+						selection_caret = { fg = "#89b4fa" },
+						selection = { bg = "#45475a", fg = "#89b4fa" },
+					},
 				},
-				["live_grep_args"] = {
-					auto_qoting = true,
+
+				pickers = {
+					-- Consistent configuration for all pickers
+					find_files = {
+						-- theme = "dropdown",
+						previewer = false,
+						layout_config = {
+							width = dimensions.width,
+							height = dimensions.height,
+						},
+					},
+					oldfiles = {
+						-- theme = "dropdown",
+						previewer = false,
+						initial_mode = "normal",
+						layout_config = {
+							width = dimensions.width,
+							height = dimensions.height,
+						},
+					},
+					buffers = {
+						-- theme = "dropdown",
+						initial_mode = "normal",
+						previewer = false,
+						layout_config = {
+							width = dimensions.width,
+							height = dimensions.height,
+						},
+					},
+					live_grep = {
+						theme = "dropdown",
+						previewer = false,
+						layout_config = {
+							width = dimensions.width,
+							height = dimensions.height,
+						},
+					},
+					git_branches = {
+						-- theme = "dropdown",
+						previewer = false,
+						layout_config = {
+							width = dimensions.width,
+							height = dimensions.height,
+						},
+					},
+					git_commits = {
+						-- theme = "dropdown",
+						initial_mode = "normal",
+						previewer = false,
+						layout_config = {
+							width = dimensions.width,
+							height = dimensions.height,
+						},
+					},
+					git_bcommits = {
+						-- theme = "dropdown",
+						initial_mode = "normal",
+						previewer = false,
+						layout_config = {
+							width = dimensions.width,
+							height = dimensions.height,
+						},
+					},
+					git_file_history = {
+						-- theme = "dropdown",
+						initial_mode = "normal",
+						layout_strategy = "vertical",
+						previewer = false,
+						layout_config = {
+							width = dimensions.width,
+							height = dimensions.height,
+						},
+					},
+				},
+
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({
+							layout_config = {
+								width = dimensions.width,
+								height = dimensions.height,
+							},
+						}),
+					},
+					["live_grep_args"] = {
+						auto_quoting = true,
+						-- theme = "dropdown",
+						previewer = false,
+						layout_config = {
+							width = dimensions.width,
+							height = dimensions.height,
+						},
+					},
 				},
 			}
 
 			telescope.setup(options)
 
-			-- Keymaps
+			-- Your existing keymaps
 			local builtin = require("telescope.builtin")
-
 			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch: [F]iles" })
 			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "[S]earch: Oldfiles " })
 			vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch: [B]uffers" })
@@ -62,6 +203,30 @@ return {
 				{ noremap = true, silent = true, desc = "[S]earch current [S]election" }
 			)
 
+			-- Git related keymaps
+			vim.keymap.set({ "n", "v" }, "<leader>sb", builtin.git_branches, { desc = "[S]earch: Git [B]ranches" })
+			vim.keymap.set({ "n", "v" }, "<leader>sc", builtin.git_commits, { desc = "[S]earch: Git [C]ommits" })
+			vim.keymap.set({ "n", "v" }, "<leader>sh", builtin.git_bcommits, { desc = "[S]earch: Git [H]istory" })
+			vim.keymap.set(
+				{ "n", "v" },
+				"<leader>sH",
+				":Telescope git_file_history<CR>",
+				{ desc = "[S]earch: Git [H]istory" }
+			)
+
+			telescope.setup({
+				extensions = {
+					git_file_history = {
+						mappings = {
+							i = { ["<C-g>"] = telescope.extensions.git_file_history.actions.open_in_browser },
+							n = { ["<C-g>"] = telescope.extensions.git_file_history.actions.open_in_browser },
+						},
+						browser_command = nil,
+					},
+				},
+			})
+
+			-- LSP related keymaps
 			vim.keymap.set(
 				{ "n", "v" },
 				"gd",
@@ -81,6 +246,7 @@ return {
 			telescope.load_extension("ui-select")
 			telescope.load_extension("live_grep_args")
 			telescope.load_extension("git_branch")
+			telescope.load_extension("git_file_history")
 		end,
 	},
 }
