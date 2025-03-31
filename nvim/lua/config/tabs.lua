36110c72987afa3cc9f314ea35fc3b447d39eeb9
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2025-03-31 23:55:05 +0200

    feat: add tab navigation via numbers

--- nvim/lua/config/tabs.lua
+++ nvim/lua/config/tabs.lua
@@ -4,3 +4,8 @@ vim.keymap.set("n", "<tab>l", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 vim.keymap.set("n", "<tab><tab>", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 vim.keymap.set("n", "<tab>h", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 vim.keymap.set("n", "<tab>b", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move
+
+-- Go to tab number with <tab> + number 1 ... 9
+for i = 1, 9, 1 do
+	vim.keymap.set("n", "<tab>" .. i, "<cmd>tabnext " .. i .. "<CR>", { desc = "Go to tab " .. i })
+end
