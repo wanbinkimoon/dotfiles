948d3de4cb81b70d68e9b51269c8860a22cb2f59
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2025-04-15 15:05:15 +0200

    refactor: remove useless commented out ckeymaps in favour of telescope ones

--- nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -10,29 +10,12 @@ return {
 		local lspconfig = require("lspconfig")
 		local icons = require("config.icons")
 
-		lspconfig.ts_ls.setup({
-			capabilities = capabilities,
-		})
-
-		lspconfig.html.setup({
-			capabilities = capabilities,
-		})
-
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-		})
-
-		lspconfig.yamlls.setup({
-			capabilities = capabilities,
-		})
-
-		lspconfig.jsonls.setup({
-			capabilities = capabilities,
-		})
-
-		lspconfig.cssls.setup({
-			capabilities = capabilities,
-		})
+		lspconfig.ts_ls.setup({ capabilities = capabilities })
+		lspconfig.html.setup({ capabilities = capabilities })
+		lspconfig.lua_ls.setup({ capabilities = capabilities })
+		lspconfig.yamlls.setup({ capabilities = capabilities })
+		lspconfig.jsonls.setup({ capabilities = capabilities })
+		lspconfig.cssls.setup({ capabilities = capabilities })
 
 		lspconfig.pyright.setup({
 			capabilities = capabilities,
@@ -181,9 +164,6 @@ return {
 		-- Basic Setup END --
 
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Definition" })
-		-- vim.keymap.set("n", "Gd", vim.lsp.buf.definition, { desc = "[C]ode: Go to [d]efinition" })
-		-- vim.keymap.set("n", "Gr", vim.lsp.buf.references, { desc = "[C]ode: Go to [R]eference" })
-		-- vim.keymap.set("n", "Gi", vim.lsp.buf.implementation, { desc = "[C]ode: Go to [i]mplementation" })
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode: [A]ction" })
 		vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "[C]ode: [R]ename" })
 	end,
