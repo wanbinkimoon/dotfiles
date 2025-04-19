9306cdcff74164d7b0eb46ac8189f59d077aa636
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2025-04-20 00:00:09 +0200

    chore: remove folding

--- nvim/lua/config/move-code.lua
+++
@@ -1,5 +0,0 @@
--- Move lines up and down
-vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==")
-vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==")
-vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv")
