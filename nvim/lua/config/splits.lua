57848abb4a1c28b9fa4dcdfe7658449a46a1e663
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2025-04-20 00:00:09 +0200

    chore: remove folding

--- nvim/lua/config/splits.lua
+++
@@ -1,4 +0,0 @@
--- Split (for some reason we need to specify the Shift key)
-vim.keymap.set("n", "sl", ":vsplit<CR>", { desc = "Split vertically" })
-vim.keymap.set("n", "sj", ":split<CR>", { desc = "Split horizontally" })
-vim.keymap.set("n", "sq", ":q<CR>", { desc = "Close pane" })
