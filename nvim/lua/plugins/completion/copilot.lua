0acb678ad6eb843950643eadda1be88905284d47
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2025-04-29 01:09:15 +0200

    feat: ai coding is now code-caompanion

--- nvim/lua/plugins/completion/copilot.lua
+++ nvim/lua/plugins/completion/copilot.lua
@@ -1,23 +1,4 @@
 return {
-	{
-		"github/copilot.vim",
-		event = { "InsertEnter", "LspAttach" },
-	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		dependencies = {
-			{ "github/copilot.vim" },
-			{ "nvim-lua/plenary.nvim", branch = "master" },
-		},
-		build = "make tiktoken", -- Only on MacOS or Linux
-		opts = {
-			model = "claude-3.7-sonnet",
-			context = "#buffer",
-		},
-		-- event = { "InsertEnter", "LspAttach" },
-		cmd = "CopilotChat",
-		keys = {
-			{ "<leader>cc", "<cmd>:CopilotChatToggle<cr>", desc = "Copilot Chat" },
-		},
-	},
+	"github/copilot.vim",
+	event = { "InsertEnter", "LspAttach" },
 }
