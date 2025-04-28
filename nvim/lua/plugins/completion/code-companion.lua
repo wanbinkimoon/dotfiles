ee460afe2bfd78705ce7d20cec3b7d59d2ae67bc
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2025-04-29 01:09:15 +0200

    feat: ai coding is now code-caompanion

---
+++ nvim/lua/plugins/completion/code-companion.lua
@@ -0,0 +1,43 @@
+return {
+	"olimorris/codecompanion.nvim",
+	event = { "InsertEnter", "LspAttach" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"echasnovski/mini.nvim",
+	},
+	keymaps = {
+		{ "<leader>cc", "<CMD>:CodeCompanionChat<CR>", desc = "Open CodeCompanion chat" },
+		{ "<leader>ci", "<CMD>:CodeCompanion<CR>", desc = "CodeCompanion inline assistant" },
+		{ "<leader>cp", "<CMD>:CodeCompanionActions<CR>", desc = "CodeCompanion actions palette" },
+		{ "<leader>ce", "<CMD>:CodeCompanion /explain<CR>", desc = "Explain code", mode = "v" },
+	},
+	opts = {
+		strategies = {
+			inline = {
+				adapter = "anthropic",
+			},
+			chat = {
+				adapter = "anthropic",
+				keymaps = {
+					send = {
+						modes = { n = "<C-a>", i = "<C-a>" },
+					},
+				},
+			},
+			display = {
+				diff = {
+					provider = "mini_diff", -- default|mini_diff
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("codecompanion").setup(opts)
+		local modes = { "n", "v" }
+		vim.keymap.set(modes, "<leader>cc", "<CMD>:CodeCompanionChat<CR>", { desc = "Open CodeCompanion chat" })
+		vim.keymap.set(modes, "<leader>ci", "<CMD>:CodeCompanion<CR>", { desc = "CodeCompanion inline assistant" })
+		vim.keymap.set(modes, "<leader>cp", "<CMD>:CodeCompanionActions<CR>", { desc = "CodeCompanion palette" })
+		vim.keymap.set(modes, "<leader>ce", "<CMD>:CodeCompanion /explain<CR>", { desc = "Explain code" })
+	end,
+}
