468084aaeb678cab4f2fa989a252ee06f8b145b6
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2025-08-01 15:07:26 +0200

    feat: speedup zsh

--- zsh/zsh-config.sh
+++ zsh/zsh-config.sh
@@ -1,7 +1,6 @@
 # Set up fzf key bindings and fuzzy completion
 source <(fzf --zsh)
 
-
 # history setup
 HISTFILE=$HOME/.zhistory
 SAVEHIST=10000
@@ -16,8 +15,14 @@ bindkey '^[[A' history-search-backward
 bindkey '^[[B' history-search-forward
 
 # completion using arrow keys (based on commands)
+ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+autoload -Uz compinit && compinit -C -u
+
+# source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $(brew --prefix)/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+ZSH_HIGHLIGHT_MAXLENGTH=300
 
 # Add zsh-completions to the shell
 zstyle ':completion:*' menu select
