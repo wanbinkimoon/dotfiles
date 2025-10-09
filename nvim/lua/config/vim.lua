3329467e4b53b1ad8e5cf8e58e8edbee2e9ca5d6
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2025-10-10 00:56:24 +0200

    fix: indentation

--- nvim/lua/config/vim.lua
+++ nvim/lua/config/vim.lua
@@ -1,7 +1,13 @@
-vim.cmd("set softtabstop=2")
-vim.cmd("set tabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set expandtab")
+-- vim.cmd("set softtabstop=2")
+-- vim.cmd("set tabstop=2")
+-- vim.cmd("set shiftwidth=2")
+-- vim.cmd("set expandtab")
+
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
