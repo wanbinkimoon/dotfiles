0de525cbfda48c960622b5d97414e207ac758bc8
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2026-04-12 23:58:22 +0200

    chore(scripts): remove from tracking

--- scripts/unblock_instagram.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to unblock Instagram by removing entry from /etc/hosts
-HOSTS_FILE="/etc/hosts"
-MARKER="# Instagram block"
-
-# Remove the Instagram block entry
-sudo sed -i '' "/$MARKER/d" "$HOSTS_FILE"
-echo "Instagram unblocked at $(date)"
