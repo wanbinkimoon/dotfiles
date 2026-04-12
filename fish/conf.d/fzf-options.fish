1ac05dbaa7c240c4d3aa4907f0ea3383f16c6992
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2026-04-12 23:48:53 +0200

    chore(fish): update config

--- fish/conf.d/fzf-options.fish
+++ fish/conf.d/fzf-options.fish
@@ -1,3 +1,13 @@
-# FZF Dracula theme 
-export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,hl:#bd93f9 --color=fg+:#f8f8f2,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
-
+set -gx FZF_DEFAULT_OPTS "
+  --height 40%
+  --layout reverse
+  --border rounded
+  --info inline
+  --prompt '❯ '
+  --pointer '▸'
+  --marker '✓'
+  --color=fg:#f8f8f2,hl:#bd93f9
+  --color=fg+:#f8f8f2,hl+:#bd93f9
+  --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6
+  --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4,border:#6272a4
+"
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,12 @@
+set -gx AWS_REGION eu-west-3
+
+## Agent scripts
+fish_add_path ~/.agents/scripts
+fish_add_path ~/.agents/scripts/ralph
+
+## Initialize CLAUDE_SOUNDS_MUTED (universal + exported)
+set -q CLAUDE_SOUNDS_MUTED; or set -Ux CLAUDE_SOUNDS_MUTED 0
+
 if status is-interactive
   source ~/.config/fish/completions/omz-git-aliases.fish
 
