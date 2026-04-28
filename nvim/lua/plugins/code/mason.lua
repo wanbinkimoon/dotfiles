146f72db19dc970e56138bb6015376807271f4f5
Nicola Bertelloni <nicola.bertelloni@qonto.com>
2026-04-28 11:28:22 +0200

    feat(nvim): add mason-tool-installer for self-bootstrap

--- nvim/lua/plugins/code/mason.lua
+++ nvim/lua/plugins/code/mason.lua
@@ -1,7 +1,41 @@
 return {
-	"williamboman/mason.nvim",
-	lazy = false,
-	config = function()
-		require("mason").setup()
-	end,
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		event = "VeryLazy",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- Language servers
+					"eslint-lsp",
+					"typescript-language-server",
+					"lua-language-server",
+					"json-lsp",
+					"yaml-language-server",
+					"html-lsp",
+					"css-lsp",
+					"emmet-language-server",
+					"marksman",
+					"graphql-language-service-cli",
+					"pyright",
+					-- Formatters
+					"oxfmt",
+					"prettierd",
+					"stylua",
+					-- Linters
+					"djlint",
+				},
+				auto_update = false,
+				run_on_start = true,
+				start_delay = 3000, -- ms: defer past startup-critical path
+			})
+		end,
+	},
 }
